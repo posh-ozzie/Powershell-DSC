03d381f4594c1997f9156ddb75bf44230856205b
Ross Halford <ross.halford@outlook.com>
2017-07-13 21:12:12 +0100

    Added  Pull Server Scripts

---
+++ DSC-HTTPS-Server.ps1
@@ -0,0 +1,50 @@
+#HTTP Pull Server
+#OK for test environment, dont use in Production
+Configuration HTTPSPullServer
+{
+    #Module Must Exist on Pull Server
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    Node WS000DSC
+    {
+        WindowsFeature DSCServiceFeature
+        {
+            Ensure = "Present"
+            Name = "DSC-Service"
+        }
+
+        #WindowsFeature IISConsole #For GUI Servers - Core Server Comment Out
+        #{
+        #    Ensure = "Present"
+        #    Name = "Web-Mgmt-Console"
+        #}
+
+        xDSCWebService PSDSCPullServer
+        {
+            Ensure = "Present"
+            EndpointName = "PSDSCPullServer"
+            Port = 8080
+            PhysicalPath = "$env:SystemDrive\inetpub\wwwroot\PSDSCPullServer" #Location of Website Files
+            CertificateThumbPrint = #Certificate Hash Here.
+            ModulePath = "$env:ProgramFiles\WindowsPowershell\DscService\Modules"
+            ConfigurationPath = "$env:ProgramFiles\WindowsPowershell\DscService\Configuration" #Configuration File Storage
+            State = "Started"
+            UseSecurityBestPractices = $true
+            DependsOn = "[WindowsFeature]DSCServiceFeature"
+        }
+        xDSCWebService PSDSCReportingServer #PSDSCComnplianceServer on the MVA Course.
+        {
+            Ensure = "Present"
+            EndpointName = "PSDSCReportingServer"
+            Port = 9080
+            PhysicalPath = "$env:SystemDrive\inetpub\wwwroot\PSDSCReportingServer" #Location of Website Files
+            CertificateThumbPrint = #Certificate Hash Here.
+            State = "Started"
+            UseSecurityBestPractices = $true
+            DependsOn = ("[WindowsFeature]DSCServiceFeature","[xDSCWebService]PSDSCPullServer")
+        }
+    }
+}
+
+#Generate MOF
+HTTPPullServer -OutputPath C:\temp\MOFs
